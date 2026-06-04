685a9522470038667d9498932e256b72cdd4f06e
David Xing <xingdavid2026@vt.edu>
2026-06-04 11:20:50 -0400

    add interactive greeting shinyApp

--- scripts/interactive-shiny-practice.R
+++ scripts/interactive-shiny-practice.R
@@ -11,18 +11,42 @@ library(tidyverse)
 # UI
 ui <- fluidPage(
   titlePanel("Interactive Greeting Application"),
-  textInput(inputId = "user_input", label = "Enter your greeting:", 
-            value = "Hello, World"),
-  textOutput(outputId = "greeting")
   
+  textInput(inputId = "user_input1", label = "Enter your greeting:", 
+            value = "Hello, World"),
+  textOutput(outputId = "greeting1"),
+  
+  selectInput(inputId = "user_input2", label = "Choose your greeting:",
+              choices = c("Hello, World", "Howdy", "What's up"),
+              selected = "What's up"),
+  textOutput(outputId = "greeting2"),
+  
+  radioButtons(inputId = "user_input3", label = "Pick your greeting:",
+              choices = c("Hello, World", "Howdy", "What's up"),
+              selected = "What's up"),
+  textOutput(outputId = "greeting3"),
+  
+  textInput(inputId = "name", label = "What is your name?"),
+  textOutput("greeting4")
 )
 
 # SERVER
 server <- function(input, output) {
-  output$greeting <- renderText({
-    paste0(input$user_input, " is the greeting you entered")
+  output$greeting1 <- renderText({
+    paste0(input$user_input1, " is the greeting you entered")
   })
   
+  output$greeting2 <- renderText({
+    paste0(input$user_input2, " is the greeting you entered")
+  })
+  
+  output$greeting3 <- renderText({
+    paste0(input$user_input3, " is the greeting you entered")
+  })
+
+  output$greeting4 <- renderText({
+    paste0(input$greeting4, " ", input$name)
+  })
 }
 
 # LAUNCH APP
